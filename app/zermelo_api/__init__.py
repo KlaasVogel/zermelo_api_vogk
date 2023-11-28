10528f26cc2ab37ebaa9dcdd02808d6286090f71
Klaas Vogel <klaasvogel@gmail.com>
2023-11-28 16:34:26 +0100

    V0.2.3

--- app/zermelo_api/__init__.py
+++ app/zermelo_api/__init__.py
@@ -2,3 +2,4 @@ from .src.zermelo_api import ZermeloAPI
 from .src.branches import Branch, Branches
 from .src.time_utils import *
 from .src.users import Leerling, Medewerker, Leerlingen, Personeel
+from .src.lesgroepen import Lesgroepen, Lesgroep
