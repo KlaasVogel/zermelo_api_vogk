aaa11f50e6c6ee27936d2e8f172a1861e5a3eee4
Klaas Vogel <klaasvogel@gmail.com>
2024-01-15 14:43:57 +0100

    V0.3.1

--- app/zermelo_api/src/vakken.py
+++ app/zermelo_api/src/vakken.py
@@ -42,6 +42,42 @@ class Vak:
 
 
 @dataclass
+class VakDocLokData:
+    subjects: list[str]
+    teachers: list[str]
+    locationsOfBranch: list[str]  # lokalen
+
+
+@dataclass
+class DataVakDocLoks(ZermeloCollection, list[VakDocLokData]):
+    id_branch: InitVar
+    start: InitVar
+    eind: InitVar
+
+    def __post_init__(self, id_branch: int, start: int, eind: int):
+        query = f"appointments?branchOfSchool={id_branch}&fields=locationsOfBranch,subjects,teachers&start={start}&end={eind}"
+        self.load_collection(query, VakDocLokData)
+
+
+@dataclass
+class VakDocLok:
+    vak: Vak
+    docenten: list[str] = field(default_factory=list)
+    lokalen: list[str] = field(default_factory=list)
+
+
+@dataclass
+class VakDocLoks(list[VakDocLok]):
+    vakken: InitVar
+    vakdata: InitVar
+
+    def __post_init__(self, vakken: list[Vak], vakdata: DataVakDocLoks):
+        for data in vakdata:
+            for vaknaam in data.subjects:
+                vak = vakken.get(vaknaam)
+
+
+@dataclass
 class Vakken(ZermeloCollection, list[Vak]):
     schoolinschoolyear: InitVar
 
@@ -49,5 +85,15 @@ class Vakken(ZermeloCollection, list[Vak]):
         query = f"choosableindepartments?schoolInSchoolYear={schoolinschoolyear}"
         self.load_collection(query, Vak)
 
+    def get(self, vaknaam: str) -> Vak:
+        for vak in self:
+            if vak.subjectCode == vaknaam:
+                return vak
+
     def get_leerjaar_vakken(self, leerjaar_id: int) -> list[Vak]:
         return [vak for vak in self if vak.departmentOfBranch == leerjaar_id]
+
+    def get_vak_docent_lokaal(self, id_branch: int, start: int, eind: int):
+        vakdata = DataVakDocLoks(id_branch, start, eind)
+        logger.info(self)
+        # return VakDocLoks(self, vakdata)
