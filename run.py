aec3aef5e0e046206f326a3723ac9ba7dae44daf
KlaasVogel <klaasvogel@gmail.com>
2024-05-28 16:07:51 +0200

    V1.0.9

--- run.py
+++ run.py
@@ -9,6 +9,8 @@ async def main():
     branches = await load_branches("carmelhengelo")
     branch = branches.get("lg")
     print(branch)
+    data = await branch.get_vak_doc_loks()
+    [logger.info(row) for row in data]
     # for branch in branches:
     #     if branch.branch == "lg":
     #         lesgroepen = await branch.find_lesgroepen()
