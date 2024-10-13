bd305db1c9a8b69db9e3eadcd541a392706c0820
KlaasVogel <klaasvogel@gmail.com>
2024-10-13 21:49:50 +0200

    V1.1.8-2

--- app/zermelo_api/__init__.py
+++ app/zermelo_api/__init__.py
@@ -5,7 +5,12 @@ from .src._time_utils import *
 from .src.users import Leerling, Medewerker, Leerlingen, Personeel
 from .src.lesgroepen import Lesgroepen, Lesgroep
 from .src.leerjaren import Leerjaar, Leerjaren
-from .src.appointments import get_user_appointments, get_department_updates, Appointment
+from .src.appointments import (
+    get_user_appointments,
+    get_department_updates,
+    get_location_appointments,
+    Appointment,
+)
 from .src.vakken import Vakken, Vak
 from .src.lokalen import Lokalen, Lokaal
 from .src.vakdoclok import VakDocLoks, VakDocLok
