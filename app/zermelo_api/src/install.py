a66056c88cb4a9b46cac84a6824c9e41c733b9b3
Klaas Vogel <klaasvogel@gmail.com>
2025-07-03 12:45:02 +0200

    V1.3.0.dev5

--- app/zermelo_api/src/install.py
+++ app/zermelo_api/src/install.py
@@ -6,14 +6,17 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(filename="example.log", encoding="utf-8", level=logging.DEBUG)
 
 
-async def main(schoolname: str, code: str):
+async def main(schoolname: str, code: str) -> bool:
     api = await loadAPI(schoolname)
     try:
         await api.login(code)
-        if api.checkCreds():
-            print("")
+        if await api.checkCreds():
+            print("token set up correctly")
+            return True
     except Exception as e:
         logger.exception(e)
+    print("error setting up credentials")
+    return False
 
 
 def installAPI(schoolname: str = "", code: str = ""):
@@ -24,7 +27,8 @@ def installAPI(schoolname: str = "", code: str = ""):
     while not code:
         code = input("code: ")
 
-    asyncio.run(main(schoolname, code))
+    if not asyncio.run(main(schoolname, code)):
+        installAPI()
 
 
 if __name__ == "__main__":
