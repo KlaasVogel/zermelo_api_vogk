53ccbba1c354159d8acf3c65e75f6570f25f3376
Klaas Vogel <klaasvogel@gmail.com>
2025-07-03 12:36:15 +0200

    V1.3.0.dev4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="zermelo_api_vogk",
-    version="1.3.0.dev3",
+    version="1.3.0.dev4",
     description="A small module to create a Zermelo accesstoken and put some data from Zermelo in dataclasses",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
